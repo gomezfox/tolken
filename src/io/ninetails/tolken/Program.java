9c99504aaa47757b32d61242ff2ca80868cfd420
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-09-02 23:50:24 -0700

    Adds Program class

---
+++ src/io/ninetails/tolken/Program.java
@@ -0,0 +1,11 @@
+package io.ninetails.tolken;
+
+import java.util.ArrayList;
+
+public class Program {
+    ArrayList<CodingUnit> codingUnits;
+    CodingUnit defaultCodingUnit;
+    public void run() {
+        // runs codingUnits
+    }
+}
