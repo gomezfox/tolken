4328a1d6fe59f9ea3f06a0a2be71e085db95ad3b
Stephen Gomez-Fox <gomezfox@uw.edu>
2020-09-02 23:52:06 -0700

    Adds Section class

--- src/io/ninetails/tolken/Program.java
+++ src/io/ninetails/tolken/Program.java
@@ -3,8 +3,8 @@ package io.ninetails.tolken;
 import java.util.ArrayList;
 
 public class Program {
-    ArrayList<CodingUnit> codingUnits;
-    CodingUnit defaultCodingUnit;
+    ArrayList<Section> sections;
+    Section defaultSection;
     public void run() {
         // runs codingUnits
     }
